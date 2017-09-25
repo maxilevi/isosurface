f7a3ace3396a5a8d2ff57a0ac1ddec028b30f6b7
Zaphyk <zaphyk@live.com>
2017-09-25 08:04:43 -0300

    fixed drag and drop on mobile

--- isosurface.js
+++ isosurface.js
@@ -247,7 +247,7 @@ for(x = 0; x < chunk_width-1; x++){
 }
 verts = result.vertices;
 norms = result.normals;
-console.log(verts);
+
 
 
   // Now pass the list of positions into WebGL to build the
@@ -350,6 +350,34 @@ function handleMouseMove(event) {
 	lastMouseX = newX
 	lastMouseY = newY;
 }
+
+init();
+
+function touchHandler(event) {
+    var touch = event.changedTouches[0];
+
+    var simulatedEvent = document.createEvent("MouseEvent");
+        simulatedEvent.initMouseEvent({
+        touchstart: "mousedown",
+        touchmove: "mousemove",
+        touchend: "mouseup"
+    }[event.type], true, true, window, 1,
+        touch.screenX, touch.screenY,
+        touch.clientX, touch.clientY, false,
+        false, false, false, 0, null);
+
+    touch.target.dispatchEvent(simulatedEvent);
+    event.preventDefault();
+}
+
+function init() {
+    document.addEventListener("touchstart", touchHandler, true);
+    document.addEventListener("touchmove", touchHandler, true);
+    document.addEventListener("touchend", touchHandler, true);
+    document.addEventListener("touchcancel", touchHandler, true);
+}
+
+
 
 //
 // Draw the scene.
